779fa6921deda45a069a00983396511924f90bc5
colebui <colebui@tamu.edu>
2020-02-08 04:24:06 +0000

    finished hw

--- rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -11,8 +11,60 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    redirect = false
+    count = 0
+    countNonRedirects = 0
+
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
+    end 
+    
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings] and not params[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+    
+    if not redirect
+      redirect = false
+      countNonRedirects = countNonRedirects + 1
+    else
+      flash.keep
+      redirect_to movies_path :sort_by=>@sort_by, :ratings=>@ratings
+    end
+    
+    if @ratings and @sort_by
+      @movies = Movie.where(:rating => @ratings.keys).order(params[:sort_by])
+    elsif @sort_by and not @ratings
+      @movies = Movie.order(params[:sort_by])
+    elsif @ratings and not @sort_by
+      @movies = Movie.where(:rating => @ratings.keys)
+    elsif not @ratings and not @sort_by
+      @movie = Movie.all
+    end
+    if @ratings
+      count = count + 1
+    else 
+      @ratings = Hash.new
+    end
+    
   end
+
 
   def new
     # default: render 'new' template
